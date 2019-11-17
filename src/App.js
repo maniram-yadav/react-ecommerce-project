3c7ce5e574d3397b18c03fb3a6968aeb6491714b
maniram-yadav <maniram.yadav@glaucuslogistics.com>
2019-11-17 14:18:08 +0530

    react ecommerce project

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import {Switch,Route} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default';
+import Modal from './components/Modal'
+
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+             <Navbar></Navbar>
+             <Switch>
+                 <Route path="/details" component={Details} ></Route>
+                 <Route path="/cart" component={Cart} ></Route>
+                 <Route path="/" exact component={ProductList} ></Route>
+                 <Route  component={Default} ></Route>
+             </Switch>
+               <Modal />
+            </React.Fragment>
+        )
+    }
+}
+
+export default App
